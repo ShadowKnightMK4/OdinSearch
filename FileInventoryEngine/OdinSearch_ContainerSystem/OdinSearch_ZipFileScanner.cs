d09921398a2c174bcf696b4976ace51fe4f9b4b9
Thomas Paul Betterly <thomasbetterly@aol.com>
2023-05-30 11:52:46 -0400

    Start Working toward implementing concept of container files.
     Files such as zip files that can be opened and checked again matches.

---
+++ FileInventoryEngine/OdinSearch_ContainerSystem/OdinSearch_ZipFileScanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdinSearchEngine.OdinSearch_FileSystemScanner
+{
+    internal class OdinSearch_ZipFileScanner
+    {
+    }
+}
