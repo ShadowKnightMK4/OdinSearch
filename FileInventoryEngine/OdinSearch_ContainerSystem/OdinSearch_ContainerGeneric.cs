581a508b23333475cca8a4ad33d1195e6d9e1d46
Thomas Paul Betterly <thomasbetterly@aol.com>
2023-05-30 21:33:15 -0400

    getting master to build with new stuff

--- FileInventoryEngine/OdinSearch_ContainerSystem/OdinSearch_ContainerGeneric.cs
+++ FileInventoryEngine/OdinSearch_ContainerSystem/OdinSearch_ContainerGeneric.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace OdinSearchEngine.OdinSearch_FileSystemScanner
+namespace OdinSearchEngine.OdinSearch_ContainerSystems
 {
 
     /// <summary>
