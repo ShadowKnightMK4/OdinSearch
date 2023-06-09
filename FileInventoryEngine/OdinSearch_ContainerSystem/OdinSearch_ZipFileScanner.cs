9dc8a2cde57f6a4e2f18f4796fe1f44e279e8b46
Thomas Paul Betterly <thomasbetterly@aol.com>
2023-06-09 08:11:38 -0400

    Work to add container stuff to the engine.

--- FileInventoryEngine/OdinSearch_ContainerSystem/OdinSearch_ZipFileScanner.cs
+++ FileInventoryEngine/OdinSearch_ContainerSystem/OdinSearch_ZipFileScanner.cs
@@ -5,76 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO.Compression;
 using System.IO;
+using System.Diagnostics;
 
 namespace OdinSearchEngine.OdinSearch_ContainerSystems
 {
-    /// <summary>
-    /// NOT FULLY IMPLEMENTED YET
-    /// </summary>
-    public class OdinSearch_ZipFileScanner: OdinSearch_ContainerGeneric
+    public class OdinSearch_ZippedFileInfo: OdinSearch_FileInfoGeneric
     {
-        protected string WalkPathForZip(string path, out string remaining)
+        public OdinSearch_ZippedFileInfo(string location):base(location)
         {
-            StringBuilder ret = new StringBuilder();
 
-            for (int step = 0; step < path.Length;step++)
-            {
-                ret.Append(path[step]);
-                if ( (path[step] == Path.DirectorySeparatorChar) || (path[step] == Path.AltDirectorySeparatorChar))
-                {
-                    try
-                    {
-                        if (File.Exists(ret.ToString()) == true)
-                        {
-                            FileInfo Info = new FileInfo(ret.ToString());   
-                            if (Info.LinkTarget != null)
-                            {
-                                ret.Clear();
-                                ret.Append(Info.LinkTarget);
-                            }
-                            if (File.Exists(ret.ToString()) == true)
-                            {
-                                
-                            }
-                        }
-                    }
-                    finally
-                    {
-
-                    }
-                }
-            }
-            remaining= string.Empty;
-            return ret.ToString();
         }
-        public override string[] GetContainerFiles(string Location)
-        {
-            string zipart;
-            string remain;
-            zipart = WalkPathForZip(Location, out remain);
-            throw new NotImplementedException();
-            using (Stream instream = File.OpenRead(zipart))
-            {
-                using (ZipArchive zipArchive = new ZipArchive(instream, ZipArchiveMode.Read, true))
-                {
-
-                }
-            }
-        }
-
-        public override string[] GetFiles(string Location)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override OdinSearch_ContainerSystemItem MakeInstance(string Name)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-        }
+        override 
+        
     }
+
+    public class OdinSearch_ZippedDirectoryInfo: OdinSearch_DirectoryInfoGeneric
+
 }
+
